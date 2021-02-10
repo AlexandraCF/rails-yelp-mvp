fedf177933f1bb1a73a342c943c18e0610e793c2
Alexandra Canuel <alexcanuel@gmail.com>
2021-02-10 13:17:06 +0100

    index show new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+maison_pillet = { name: "Maison Pillet", address: "3 rue Ampère, 75017 Paris", phone_number: "0143434343", category: "french" }
+pizzeria_joe = { name: "Pizzera Joe", address: "19 rue Biot, 75017 Paris", phone_number: "0144444444", category: "italian" }
+taykedo = { name: "Taykedo", address: "15 rue de la Terrasse, 75017 Paris", phone_number: "0145555555", category: "japanese" }
+brasserie_bond = { name: "Brasserie Bond", address: "28 avenue de Villiers, 75017 Paris", phone_number: "0146666666", category: "belgian" }
+chez_ly = { name: "Chez Ly", address: "58 avenue de Niel, 75017 Paris", phone_number: "0146666666", category: "chinese" }
+
+
+[ maison_pillet, pizzeria_joe, taykedo, brasserie_bond, chez_ly ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
